42abcd53f82f062d77174d40e7d5cbfca660ca44
yijiejiang <yijiejiang@live.com>
2018-09-27 15:26:25 +0800

    add MyWork update kNN.py from iteritems to items have a try add some notes

--- Ch02/MyWork.py
+++ Ch02/MyWork.py
@@ -2,40 +2,40 @@ from numpy import *
 import operator
 from os import listdir
 
-def classify0(inX, dataSet, labels, k):
-    dataSetSize = dataSet.shape[0]
-    diffMat = tile(inX, (dataSetSize,1)) - dataSet
+def classify0(inX, dataSet, labels, k):     # kNN主要算法实现过程
+    dataSetSize = dataSet.shape[0]      # 找出dataSet的大小
+    diffMat = tile(inX, (dataSetSize, 1)) - dataSet      # 计算过程
     sqDiffMat = diffMat**2
     sqDistances = sqDiffMat.sum(axis=1)
     distances = sqDistances**0.5
-    sortedDistIndicies = distances.argsort()
+    sortedDistIndicies = distances.argsort()    # 将计算结果排序
     classCount={}
-    for i in range(k):
+    for i in range(k):      # 确定K值
         voteIlabel = labels[sortedDistIndicies[i]]
-        classCount[voteIlabel] = classCount.get(voteIlabel,0) + 1
+        classCount[voteIlabel] = classCount.get(voteIlabel, 0) + 1
     sortedClassCount = sorted(classCount.items(), key=operator.itemgetter(1), reverse=True)
     return sortedClassCount[0][0]
 
-def img2vector(filename):
-    returnVect = zeros((1,1024))
+def img2vector(filename):   # 处理图片函数
+    returnVect = zeros((1, 1024))
     fr = open(filename)
     for i in range(32):
         lineStr = fr.readline()
         for j in range(32):
-            returnVect[0,32*i+j] = int(lineStr[j])
+            returnVect[0, 32*i+j] = int(lineStr[j])     # 将矩阵转换成一维向量
     return returnVect
 
 def handwritingClassTest():
     hwLabels = []
     trainingFileList = listdir('trainingDigits')           #load the training set
     m = len(trainingFileList)
-    trainingMat = zeros((m,1024))
+    trainingMat = zeros((m, 1024))      # 构造m行，1024列形状的训练集矩阵
     for i in range(m):
         fileNameStr = trainingFileList[i]
         fileStr = fileNameStr.split('.')[0]     #take off .txt
         classNumStr = int(fileStr.split('_')[0])
         hwLabels.append(classNumStr)
-        trainingMat[i,:] = img2vector('trainingDigits/%s' % fileNameStr)
+        trainingMat[i, :] = img2vector('trainingDigits/%s' % fileNameStr)
     testFileList = listdir('testDigits')        #iterate through the test set
     errorCount = 0.0
     mTest = len(testFileList)
@@ -46,7 +46,7 @@ def handwritingClassTest():
         vectorUnderTest = img2vector('testDigits/%s' % fileNameStr)
         classifierResult = classify0(vectorUnderTest, trainingMat, hwLabels, 3)
         print("the classifier came back with: %d, the real answer is: %d" % (classifierResult, classNumStr))
-        if (classifierResult != classNumStr): errorCount += 1.0
+        if (classifierResult != classNumStr): errorCount += 1.0     # 计数测试误差
     print("\nthe total number of errors is: %d" % errorCount)
     print("\nthe total error rate is: %f" % (errorCount/float(mTest)))
 
