bd8661f18b201ad7e450c127f9fa54e821cf01bb
yijiejiang <yijiejiang@live.com>
2018-10-04 17:01:25 +0800

    start chapter 3 运行程序清单3-1

--- Ch03/MyWork.py
+++ Ch03/MyWork.py
@@ -13,13 +13,18 @@ def createDataSet():
 
 def calcShannonEnt(dataSet):
     numEntries = len(dataSet)
-    labelCounts = {}
+    labelCounts = {}    # 声明一个变量保存实例总数，建立一个数据字典
     for featVec in dataSet: #the the number of unique elements and their occurance
-        currentLabel = featVec[-1]
-        if currentLabel not in labelCounts.keys(): labelCounts[currentLabel] = 0
+        currentLabel = featVec[-1]      # 将最后一个数值作为键值保存
+        if currentLabel not in labelCounts.keys():
+            labelCounts[currentLabel] = 0
         labelCounts[currentLabel] += 1
     shannonEnt = 0.0
     for key in labelCounts:
-        prob = float(labelCounts[key])/numEntries
+        prob = float(labelCounts[key])/numEntries       # prob只是过渡代数形式
         shannonEnt -= prob * log(prob,2) #log base 2
     return shannonEnt
+
+myDat, labels = createDataSet()
+print(myDat)
+print(calcShannonEnt(myDat))
