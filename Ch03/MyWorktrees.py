72dbb7e0282cac5f59050913e2f95c6bc433e31c
yijiejiang <yijiejiang@live.com>
2018-10-05 11:36:20 +0800

    完成一次阅读

--- Ch03/MyWorktrees.py
+++ Ch03/MyWorktrees.py
@@ -1,30 +1,121 @@
 from math import log
 import operator
+import matplotlib.pyplot as plt
+
 
 def createDataSet():
     dataSet = [[1, 1, 'yes'],
                [1, 1, 'yes'],
                [1, 0, 'no'],
                [0, 1, 'no'],
+               [0, 1, 'no'],
                [0, 1, 'no']]
-    labels = ['no surfacing','flippers']
+    labels = ['no surfacing', 'flippers']
     #change to discrete values
     return dataSet, labels
 
 def calcShannonEnt(dataSet):
-    numEntries = len(dataSet)
+    numEntries = len(dataSet)   # 计算dataset的长度
     labelCounts = {}    # 声明一个变量保存实例总数，建立一个数据字典
-    for featVec in dataSet: #the the number of unique elements and their occurance
+    for featVec in dataSet:     # the the number of unique elements and their occurance，循环dataset里面的每一个向量
         currentLabel = featVec[-1]      # 将最后一个数值作为键值保存
-        if currentLabel not in labelCounts.keys():
-            labelCounts[currentLabel] = 0
-        labelCounts[currentLabel] += 1
-    shannonEnt = 0.0
+        if currentLabel not in labelCounts.keys():  # 判断，如果键值没有在之前新建的数据字典之中
+            labelCounts[currentLabel] = 0   # 将currentLabel作为新值记数为0,
+        labelCounts[currentLabel] += 1  # 将currentLabel记数加一
+    shannonEnt = 0.0    # 将香农熵值初始化为0
     for key in labelCounts:
         prob = float(labelCounts[key])/numEntries       # prob只是过渡代数形式
-        shannonEnt -= prob * log(prob,2) #log base 2
+        shannonEnt -= prob * log(prob, 2) #log base 2
     return shannonEnt
 
 myDat, labels = createDataSet()
-print(myDat)
-print(calcShannonEnt(myDat))
+# print(myDat)
+# print(labels)
+# print(calcShannonEnt(myDat))
+
+def splitDataSet(dataSet, axis, value):
+    retDataSet = []     # 定义一个数据列表
+    for featVec in dataSet:     # 遍历dataSet
+        if featVec[axis] == value:  # 将符合特征的数据集抽取出来
+            reducedFeatVec = featVec[:axis]     #chop out axis used for splitting
+            reducedFeatVec.extend(featVec[axis+1:])
+            retDataSet.append(reducedFeatVec)
+    return retDataSet
+
+def chooseBestFeatureToSplit(dataSet):
+    numFeatures = len(dataSet[0]) - 1      #the last column is used for the labels
+    baseEntropy = calcShannonEnt(dataSet)   # 计算了整个数据集的原始香农熵
+    bestInfoGain = 0.0; bestFeature = -1
+    for i in range(numFeatures):        #iterate over all the features
+        featList = [example[i] for example in dataSet]#create a list of all the examples of this feature
+        uniqueVals = set(featList)       #get a set of unique values
+        newEntropy = 0.0
+        for value in uniqueVals:
+            subDataSet = splitDataSet(dataSet, i, value)
+            prob = len(subDataSet)/float(len(dataSet))
+            newEntropy += prob * calcShannonEnt(subDataSet)
+        infoGain = baseEntropy - newEntropy     #calculate the info gain; ie reduction in entropy
+        if (infoGain > bestInfoGain):       #compare this to the best gain so far
+            bestInfoGain = infoGain         #if better than current best, set to best
+            bestFeature = i
+    return bestFeature                      #returns an integer
+
+def majorityCnt(classList):
+    classCount={}
+    for vote in classList:
+        if vote not in classCount.keys(): classCount[vote] = 0
+        classCount[vote] += 1
+    sortedClassCount = sorted(classCount.items(), key=operator.itemgetter(1), reverse=True)
+    return sortedClassCount[0][0]
+
+def createTree(dataSet,labels):
+    classList = [example[-1] for example in dataSet]
+    if classList.count(classList[0]) == len(classList):
+        return classList[0]#stop splitting when all of the classes are equal
+    if len(dataSet[0]) == 1: #stop splitting when there are no more features in dataSet
+        return majorityCnt(classList)
+    bestFeat = chooseBestFeatureToSplit(dataSet)
+    bestFeatLabel = labels[bestFeat]
+    myTree = {bestFeatLabel:{}}
+    del(labels[bestFeat])
+    featValues = [example[bestFeat] for example in dataSet]
+    uniqueVals = set(featValues)
+    for value in uniqueVals:
+        subLabels = labels[:]       #copy all of labels, so trees don't mess up existing labels
+        myTree[bestFeatLabel][value] = createTree(splitDataSet(dataSet, bestFeat, value),subLabels)
+    return myTree
+
+myTree = createTree(myDat, labels)
+print(myTree)
+
+
+def classify(inputTree, featLabels, testVec):
+    firstStr = inputTree.keys()[0]
+    secondDict = inputTree[firstStr]
+    featIndex = featLabels.index(firstStr)
+    key = testVec[featIndex]
+    valueOfFeat = secondDict[key]
+    if isinstance(valueOfFeat, dict):
+        classLabel = classify(valueOfFeat, featLabels, testVec)
+    else:
+        classLabel = valueOfFeat
+    return classLabel
+
+
+def storeTree(inputTree, filename):
+    import pickle
+    fw = open(filename, 'w')
+    pickle.dump(inputTree, fw)
+    fw.close()
+
+
+def grabTree(filename):
+    import pickle
+    fr = open(filename)
+    return pickle.load(fr)
+
+fr = open('lenses.txt')
+lenses = [inst.strip().split('\t') for inst in fr.readlines()]
+lensesLabels = ['age', 'prescript', 'astigmatic', 'tearRate']
+lensesTree = createTree(lenses, lensesLabels)
+print(lensesTree)
